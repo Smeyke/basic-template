f9953ba00ed57857d7c9cf857c194943e2d71738
Michael Weibel <mweibel@imac17.local>
2016-02-29 14:24:54 +0100

    some stuff

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,7 +1,7 @@
 //  alert('Hello, World!')
 
 window.onscroll = function() {
-  var el = document.getElementsByIdName('fluid-header')[0];
+  var el = document.getElementsById('header-wrapper')[0];
   var className = 'small';
   if (el.classList) {
     if (window.scrollY > 10)
